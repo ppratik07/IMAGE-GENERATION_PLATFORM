9e73d1aa7a4af96cf0469c46339117a9a2f72ded
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-02-21 11:56:12 +0530

    Updating the schemas

--- image-generating-platform/packages/common/types.ts
+++ image-generating-platform/packages/common/types.ts
@@ -1,1 +1,22 @@
-import 'z' for "zod";
+import {z} from 'zod';
+
+export const TrainModel = z.object({
+    name : z.string(),
+    type: z.enum(["Man","Women","Other"]),
+    age : z.number(),
+    ethinicity: z.enum(["White","Black","Asian American","East Asian","South East Asian","South Asian","Middle Eastern","Pacific","Hispainc"]),
+    eyeColor : z.enum(["Brown","Black","Blue","Hazal","Gray"]),
+    bald : z.boolean(),
+    image : z.array(z.string())
+});
+
+export const GenerateImage = z.object({
+    prompt : z.string(),
+    modelId : z.string(),
+    num : z.number()
+})
+
+export const GenerateImagesFromPack = z.object({
+    modelId : z.string(),
+    packId : z.string()
+})
