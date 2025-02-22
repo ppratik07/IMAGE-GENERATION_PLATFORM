3b37904b0f4486c007e69b89906cb6a06daade4b
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-02-23 00:07:35 +0530

    Fixing types

--- image-generating-platform/packages/common/types.ts
+++ image-generating-platform/packages/common/types.ts
@@ -2,10 +2,10 @@ import {z} from 'zod';
 
 export const TrainModel = z.object({
     name : z.string(),
-    type: z.enum(["Man","Women","Other"]),
+    type: z.enum(["Man","Women","Others"]),
     age : z.number(),
-    ethinicity: z.enum(["White","Black","Asian_American","East_Asian","South_East_Asian","South_Asian","Middle_Eastern","Pacific","Hispainc"]),
-    eyeColor : z.enum(["Brown","Black","Blue","Hazal","Gray"]),
+    ethinicity: z.enum(["White","Black","Asian_American","East_Asian","South_East_Asian","South_Asian","Middle_Eastern","Pacific","Hispanic"]),
+    eyeColor : z.enum(["Brown","Black","Blue","Hazel","Gray"]),
     bald : z.boolean(),
     image : z.array(z.string())
 });
